0bc78c38576f81869ba42ddd4ee292e3b8c06aee
gabrielyeung <knigerca@gmail.com>
2025-06-23 14:57:03 -0400

    fix package config

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,6 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
-import { CrawlService } from './selenium/services/crawlService';
 import { registerKeywordIpcHandlers } from './ipc/keywords';
 import { registerCrawlIpcHandlers } from './ipc/crawl';
 import { registerWindowHandlers } from './ipc/window';
@@ -21,10 +20,9 @@ async function createWindow() {
         }
     });
 
-    // win.loadURL(`file://${path.join(__dirname, '../dist/job-crawler-ui/browser/index.html')}`);
     // Assumes Angular's built output is at dist/job-crawler-ui/index.html
-    const indexPath = path.join(__dirname, '../dist/job-crawler-ui/browser/index.html');
-    win.loadFile(indexPath);
+    win.loadURL(`file://${path.join(__dirname, '../dist/job-crawler-ui/browser/index.html')}`);
+    win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
